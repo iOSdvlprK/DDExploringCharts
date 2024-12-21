ada1359b95ce3d48067afe3124af6a68f1d60504
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-12-21 23:55:33 +0900

    Initial Commit

---
+++ DDExploringCharts/DDExploringChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDExploringChartsApp.swift
+//  DDExploringCharts
+//
+//  Created by joe on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct DDExploringChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

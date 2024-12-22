ecfec38bed880af4efa6ac17d77a2415dbca10c1
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-12-22 22:35:45 +0900

    color extension and arc view

---
+++ DDExploringCharts/View/Component Views/Colorful Button View/RandomArcFromColorListView.swift
@@ -0,0 +1,18 @@
+//
+//  RandomArcFromColorListView.swift
+//  DDExploringCharts
+//
+//  Created by joe on 12/22/24.
+//
+
+import SwiftUI
+
+struct RandomArcFromColorListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomArcFromColorListView()
+}
---
+++ DDExploringCharts/View/Component Views/Colorful Button View/TestColorButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  TestColorButtonView.swift
+//  DDExploringCharts
+//
+//  Created by joe on 12/22/24.
+//
+
+import SwiftUI
+
+struct TestColorButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestColorButtonView()
+}
